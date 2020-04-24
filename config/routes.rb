86c49978d2cce6d7cdda8308ea54703a1a63c434
PraveenRL <rlppraveen@gmail.com>
2020-04-24 10:37:36 +0530

    Show article with Id

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
-  root 'articles#show'
   get 'about', to: 'pages#about'
+
+
+  resources :articles, only: [:show]
 end
 
 # rails routes --expanded   -> show routes like table
