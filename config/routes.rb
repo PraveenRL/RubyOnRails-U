350204c8c113c4702c38b09eb332e01ded84cd07
PraveenRL <rlppraveen@gmail.com>
2020-04-24 11:15:36 +0530

    Article list (index)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
 
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end
 
 # rails routes --expanded   -> show routes like table
