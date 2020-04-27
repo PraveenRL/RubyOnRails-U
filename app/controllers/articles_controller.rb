3b3362f71d7b18a91d92d791b4ccbac859c4fd2e
PraveenRL <rlppraveen@gmail.com>
2020-04-27 08:53:22 +0530

    edit, update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,11 @@ class ArticlesController < ApplicationController
     def new 
         @article = Article.new
     end
+        
+    def edit
+        # byebug  #debug      cmd: continue 
+        @article = Article.find(params[:id])
+    end
 
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
@@ -19,6 +24,16 @@ class ArticlesController < ApplicationController
             redirect_to @article
         else
             render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
 
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
 
 # rails routes --expanded   -> show routes like table
