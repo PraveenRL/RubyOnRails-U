9e2e60e87d4b381e9eba5fdd788acddf41676a0d
PraveenRL <rlppraveen@gmail.com>
2020-04-27 08:12:57 +0530

    create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
 
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 end
 
 # rails routes --expanded   -> show routes like table
