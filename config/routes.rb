2e64d3a25b0d0258d580158eaf01978811e8f325
PraveenRL <rlppraveen@gmail.com>
2020-04-29 13:18:39 +0530

    Signup Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
 
 # rails routes --expanded   -> show routes like table
