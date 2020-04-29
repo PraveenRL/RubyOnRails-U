133854050ba5ef5c7445aa498827deafff09f4bc
PraveenRL <rlppraveen@gmail.com>
2020-04-29 21:21:03 +0530

    Login Page

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
   resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
 
 # rails routes --expanded   -> show routes like table
